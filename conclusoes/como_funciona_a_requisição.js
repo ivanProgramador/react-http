49e5874c93376badd8843b3be15d423067f3fdc5
ivan <ivan-guita@hotmail.com>
2023-10-18 20:51:58 -0300

    função post concluida

--- conclusoes/como_funciona_a_requisição.js
+++ conclusoes/como_funciona_a_requisição.js
@@ -84,5 +84,63 @@ componentDidMount(){
 }
 
 
-/*metodo post */
+/*metodo post
+  O metodo post esta sendo ezecutado de de uma funcção chamada
+  onSubmitHandler(event) qu espera o submit acontecer recebe o evento e 
+  impede que ele siga para a proxima pagina sem de fato executar o post
+*/
+
+
+onSubmitHandler(event) {
+  event.preventDefault()
+
+  /* Aqui eu declaro uma variavel 
+     que pegar todos os dados que estão dentro do 
+     state na posição usuarios
+  */
+
+  const usuario = this.state.usuario;
+
+  /* Agora eu faço um fetch na url da api pegando todos os ususrios que vem dela
+     por essa rota so que a função fetch recebe 2 parametros 
+     1 É a url da requisição.
+
+     2 É um objeto com dados da requisição
+       que eu quero fazer ele exige que o objeto tenha 3 informações 
+       
+       method -> por padrão a requisição feita com um fetch é get 
+                 se eu quiser fazer um post eu tenho que avisar nesse objeto 
+                 por isso method:'POST'
+
+       headers -> essa parte de cabeçalho é sobre oque eu vou mandar 
+                  nesse caso eu disse que vou mandar para applicação 
+                  um json mas tem api que recebe outras coisas. por 
+                  isso  headers: {'Content-Type':'application/json' }
+
+        body -> aqui eo corpo da requisição onde defato os dado esta  
+                a variavel usuario ja vem pronta do formulario porem
+                eu não posso enviar ela de qualquer jeito a api esta
+                esperando um json por isso eu uso o conversor pra 
+                tranformar o objetoque vem do formulario em um json,
+                 body: JSON.stringify(usuario)
+        
+        Essa e a estrutura necessaria para fazer um post      
+      
+  
+  */
+
+    fetch('https://reqres.in/api/users',{
+      method:'POST',
+      headers: {'Content-Type':'application/json' },
+      body: JSON.stringify(usuario)
+      }
+    )
+    .then(resposta=>resposta.json())
+    .then(dados=>{
+      console.log(dados);
+      this.setState(INITIAL_STATE)
+      this.props.adicionarUsuario(dados)
+
+    })
+ }
 
